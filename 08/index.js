a19188ac651df866b06508f4c85a62995ab4732d
DmitryWL <hedgehogwithoutlegs@gmail.com>
2022-04-02 00:28:34 +0800

    Изменил задание 08

--- 08/index.js
+++ 08/index.js
@@ -1,26 +1,5 @@
 function sum(a, b) {
-    let aArr = a.toString().split(""),
-        bArr = b.toString().split("");
-
-    aArr.reverse();
-    bArr.reverse();
-
-    let result = aArr.length > bArr.length ? aArr : bArr,
-        secondNum = aArr.length > bArr.length ? bArr : aArr;
-    for (let i = 0; i < result.length; i++) {
-        if (!secondNum[i]) break;
-        let sumNums = +result[i] + +secondNum[i];
-        result[i] = sumNums;
-    }
-
-    result.forEach((value, index) => {
-        if (+value >= 10) {
-            result[index] = +value - 10;
-            result[index + 1] = !isNaN(+result[index + 1]) ? +result[index + 1] + 1 : 1;
-        }
-    });
-
-    return result.reverse().join("");
+    return (BigInt(a) + BigInt(b)).toString();
 }
 
 module.exports = sum;
